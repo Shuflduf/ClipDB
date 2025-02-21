a8b1e0e7d201b8c888091a9059cba7d91027a863
Shuflduf <shuflduf@gmail.com>
2025-02-21 00:24:24 -0700

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,35 @@
 <script lang="ts">
   import { Client, Storage, ID, Account } from "appwrite";
+  import { onMount } from "svelte";
 
   const client = new Client()
     .setEndpoint("https://cloud.appwrite.io/v1")
     .setProject("67b777e00031574783b9");
 
   const account = new Account(client);
-  const result = account.createEmailPasswordSession(
-    "shuflduf@gmail.com",
-    "password",
-  );
+  let isAuthenticated = false;
+
+  onMount(() => {
+    account.createEmailPasswordSession("shuflduf@gmail.com", "password").then(
+      function (response) {
+        isAuthenticated = true;
+        console.log("Login successful");
+      },
+      function (error) {
+        console.log("Login failed");
+      },
+    );
+  });
 
   const storage = new Storage(client);
 
   function submit(event: Event) {
     event.preventDefault();
+    if (!isAuthenticated) {
+      console.log("User is not authenticated");
+      return;
+    }
+
     const fileInput = document.getElementById("uploader") as HTMLInputElement;
     const file = fileInput?.files?.[0] ?? new File([], "empty");
 
