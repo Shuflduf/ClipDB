f5fb372969faa0a7d64a4c39a87e4b79348d8771
Shuflduf <shuflduf@gmail.com>
2025-02-26 13:23:10 -0700

    Add title to layout for improved page identification

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+  import "../app.css";
+  let { children } = $props();
 </script>
 
+<head>
+  <title>Clip DB</title>
+</head>
 {@render children()}
